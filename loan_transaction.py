c866f63aec3553b71b2e4e2a1122e06259386760
Audakel Bit <audakel@gmail.com>
2016-01-18 11:36:33 -0700

    principal paid working

--- loan_transaction.py
+++ loan_transaction.py
@@ -16,7 +16,13 @@ auth_token["Authorization"] = "Basic %s" %auth_res['base64EncodedAuthenticationK
 err_count = 0
 total_count = 0
 loans = requests.get(API_URL + '/loans?limit=0', headers=auth_token, verify=False).json()
+
 loans = {l['externalId']: l['id'] for l in loans['pageItems']}
+DATE_FORMAT = "dd MMMM yyyy"
+LOCALE = "en"
+
+def formatDate(date):
+    return datetime.strftime(datetime.strptime(date, '%d/%m/%Y'), '%d %B %Y')
 
 def extract_other(history):
     return [x for x in history if not x._type.startswith('FEE')]
@@ -27,31 +33,51 @@ def extract_fee_sum(history):
 def extract_repayments(history):
     return [x for x in history if 'REPAYMENT' in x._type]
 
-def errHandle(res, parent):
+def cleanHistory(historyDirty):
+    reversalKeys = [x.reversalKey for x in historyDirty if x.reversalKey]
+    return [x for x in historyDirty if (not x.reversalKey and x.key not in reversalKeys)]
+
+def errHandle(res, parent, transaction=''):
     global total_count
     global err_count
-    
     total_count += 1
     if 'errors' in res.keys():
         err_count += 1
+        print('******************************')
         print(res)
+        print()
         print('Mambu Loan Key: {}'.format(parent))
+        print(transaction)
         print('Errors :{} - Total: {} | {}'.format(err_count, total_count, ((total_count-err_count)/total_count) * 100))
+        print('******************************\n')
         sys.stdout.flush()
     else:
-        print('.', end="")
-        sys.stdout.flush()
- 
+        if transaction:
+            print('transaction ({})'.format(transaction._type))
+        else:
+            print('fee')
+        # print('.', end="")
+        # sys.stdout.flush()
 
+
+def handle_repayment(transaction):
+   return res
+
+
+def handle_fee(transaction):
+    print('fee')
+       
             
-def process_loan(history):
+def process_loan(historyDirty):
     # look up the loan somehow :p
-    if len(history) < 1:
+    if len(historyDirty) < 1:
         return
+
     # skip the first row
-    if history[0].parent == 'PARENTACCOUNTKEY':
+    if historyDirty[0].parent == 'PARENTACCOUNTKEY':
         return
-    parent = history[0].parent
+
+    parent = historyDirty[0].parent
     try:
         loanid = loans[parent] # look up mifos id with externalId
     except KeyError as e:
@@ -59,67 +85,88 @@ def process_loan(history):
         print("Can't find key: %s " %parent)
         return
     
+    history = cleanHistory(historyDirty)
+
     fees = extract_fee_sum(history)
     if not isclose(fees, 0):
-        feeDate = requests.get(API_URL + '/loans/{}?limit=0&associations=repaymentSchedule'.format(loanid), 
+        feeDate = requests.get(API_URL + '/loans/{}?limit=0&associations=repaymentSchedule'.format(loanid),
             headers=auth_token, verify=False).json()['repaymentSchedule']['periods'][-1]['dueDate']
-        
         feeDateFormated = '{}-{}-{}'.format(feeDate[0], feeDate[1], feeDate[2])
         # migrate fees
         data = {
-            "locale": "en",
-            "dateFormat": "dd MMMM yyyy",
+            "locale": LOCALE,
+            "dateFormat": DATE_FORMAT,
             "amount": fees,
             "dueDate": datetime.strftime(datetime.strptime(feeDateFormated, '%Y-%m-%d'), '%d %B %Y'),
             "chargeId": "5"
         }
-        res = requests.post(API_URL+ '/loans/{}/charges'.format(loanid), headers=auth_token, json=data, verify=False).json()
+        res = requests.post(API_URL + '/loans/{}/charges'.format(loanid), headers=auth_token, json=data, verify=False).json()
         errHandle(res, parent)
 
     transactions = extract_other(history)
+    # transactions = history
+    print('**processing loan ({}T)**: {}'.format(len(transactions), parent))
     last_repayment = {}
     res = {}
     for transaction in transactions:
-        # migrate repayment
-        if 'REPAYMENT' in transaction._type:
-            if transaction._type == 'REPAYMENT':
-                data = {
-                    "locale": "en",
-                    "dateFormat": "dd MMMM yyyy",
-                    "paymentTypeId": '1',
-                    "transactionAmount": transaction.amount,
-                    "transactionDate": datetime.strftime(datetime.strptime(transaction.creation, '%Y-%m-%d'), '%d %B %Y')
-                }
-                res = requests.post(API_URL+ '/loans/{}/transactions?command=repayment'.format(loanid), headers=auth_token, json=data, verify=False).json()
-                last_repayment = {'id': res['resourceId'], 'amount': transaction.amount}
-            else:
-                data = {
-                    "locale": "en",
-                    "dateFormat": "dd MMMM yyyy",
-                    "transactionDate": datetime.strftime(datetime.strptime(transaction.creation, '%Y-%m-%d'), '%d %B %Y'),
-                    "transactionAmount": (Decimal(last_repayment['amount']) + Decimal(transaction.amount)).quantize(Decimal(10) ** -3)
-                }
-                res = requests.post(API_URL+ '/loans/{}/transactions/{}'.format(loanid, last_repayment['id']), headers=auth_token, json=data, verify=False).json()
+        if transaction._type == 'REPAYMENT' or transaction._type == 'DEFERRED_INTEREST_PAID':
+            data = {
+                "locale": LOCALE,
+                "dateFormat": DATE_FORMAT,
+                "paymentTypeId": '1',
+                "transactionAmount": transaction.amount,
+                "transactionDate": formatDate(transaction.creation)
+            }
+            res = requests.post(API_URL+ '/loans/{}/transactions?command=repayment'.format(loanid), headers=auth_token, json=data, verify=False).json()
+            # last_repayment = {'id': res['resourceId'], 'amount': transaction.amount}
+
+        # elif transaction._type == 'DEFERRED_INTEREST_PAID':
+        #     data = {
+        #         "locale": LOCALE,
+        #         "dateFormat": DATE_FORMAT,
+        #         "transactionDate": formatDate(transaction.creation),
+        #         "transactionAmount": transaction.amount,
+        #     }
+        #     res = requests.post(API_URL+ '/loans/{}/transactions?command=waiveInterest'.format(loanid), headers=auth_token, json=data, verify=False).json()
+
+        elif 'REPAYMENT' in transaction._type:
+            data = {
+                "locale": LOCALE ,
+                "dateFormat": DATE_FORMAT,
+                "transactionDate": formatDate(transaction.creation),
+                "transactionAmount": (Decimal(last_repayment['amount']) + Decimal(transaction.amount)).quantize(Decimal(10) ** -3)
+            }
+            res = requests.post(API_URL+ '/loans/{}/transactions/{}'.format(loanid, last_repayment['id']), headers=auth_token, json=data, verify=False).json()
+    
+
+        elif transaction._type == 'INTEREST_APPLIED':
+            ('handle for {}'.format(transaction._type))
+        elif transaction._type == 'DEFERRED_INTEREST_PAID':
+            ('handle for {}'.format(transaction._type))
+        elif transaction._type == 'DEFERRED_INTEREST_APPLIED':
+            ('handle for {}'.format(transaction._type))
         else:
-            if transaction._type == 'INTEREST_APPLIED':
-                pass
-            elif transaction._type == 'DEFERRED_INTEREST_PAID':
+            ('no handle for {}'.format(transaction._type))
+        errHandle(res, parent, transaction) 
+            # if transaction._type == 'INTEREST_APPLIED':
+            #     pass
+            # elif transaction._type == 'DEFERRED_INTEREST_PAID':
                 
-            elif transaction._type == 'DEFERRED_INTEREST_APPLIED':
+            # elif transaction._type == 'DEFERRED_INTEREST_APPLIED':
             
-            elif transaction._type == 'INTEREST_REDUCTION_ADJUSTMENT':
+            # elif transaction._type == 'INTEREST_REDUCTION_ADJUSTMENT':
             
-            elif transaction._type == 'DEFERRED_INTEREST_PAID_ADJUSTMENT':
+            # elif transaction._type == 'DEFERRED_INTEREST_PAID_ADJUSTMENT':
             
-            elif transaction._type == 'INTEREST_DUE_REDUCED':
+            # elif transaction._type == 'INTEREST_DUE_REDUCED':
             
-            elif transaction._type == 'DEFERRED_INTEREST_APPLIED_ADJUSTMENT':
+            # elif transaction._type == 'DEFERRED_INTEREST_APPLIED_ADJUSTMENT':
             
-            elif transaction._type == 'INTEREST_APPLIED_ADJUSTMENT':
+            # elif transaction._type == 'INTEREST_APPLIED_ADJUSTMENT':
             
-            else:
-                print ('Not covered lel')
-        errHandle(res, parent)
+            # else:
+            #     print ('Not covered lel')
+        
         
 
 #0: if(l.TYPE='REPAYMENT','REPAYMENT','FEE')
@@ -128,22 +175,23 @@ def process_loan(history):
 #3: l.AMOUNT
 #4: l.CREATIONDATE
 class Transaction():
-    def __init__(self, _type, key, parent, amount, creation):
+    def __init__(self, _type, key, parent, amount, creation, reversalKey):
         self._type = _type
         self.key = key
         self.parent = parent
         self.amount = amount
         self.creation = creation
+        self.reversalKey = reversalKey
     def __str__(self):
-        return '{}, {}, {}, {}, {}'.format(self._type, self.key,  self.parent, self.amount, self.creation)
+        return '{}, {}, {}, {}, {}, revKey:{}'.format(self._type, self.key,  self.parent, self.amount, self.creation, self.reversalKey)
     def __repr__(self):
-        return '{}, {}, {}, {}, {}'.format(self._type, self.key,  self.parent, self.amount, self.creation)
+        return '{}, {}, {}, {}, {}, revKey:{}'.format(self._type, self.key,  self.parent, self.amount, self.creation, self.reversalKey)
     def __unicode__(self):
-        return '{}, {}, {}, {}, {}'.format(self._type, self.key,  self.parent, self.amount, self.creation)
+        return '{}, {}, {}, {}, {}, revKey:{}'.format(self._type, self.key,  self.parent, self.amount, self.creation, self.reversalKey)
         
 def main():
 
-    with open('loan_repayments_all.csv') as c, concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    with open('testingLoanAccounts.csv') as c, concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
         lines = csv.reader(c, delimiter=',', quotechar='"')
         current_parent = ''
         history = []
@@ -151,8 +199,10 @@ def main():
             tran = Transaction(*line)
             if current_parent != tran.parent:
                 # change the following line to process_loan(history) to remove threading
-                executor.submit(process_loan, history)
-                #process_loan(history)
+                # executor.submit(process_loan, history)
+                print('calling proc lona with {} transactions'.format(len(history)))
+                process_loan(history)
+                print('gotback')
                 history = []
             current_parent = tran.parent
             ignore = []
